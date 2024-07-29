ee615a8ea58d246ec6e3fc0c63ba5074890410e1
Juan Ramón Sixto Anello <juan.sixto@netexlearning.net>
2024-07-29 23:48:10 +0200

    chore(aws-apigateway-resource): add support of endpoint type and timeout limit in REST

--- aws-apigateway-resource/main.tf
+++ aws-apigateway-resource/main.tf
@@ -24,7 +24,7 @@ resource "aws_api_gateway_method" "methods" {
   rest_api_id   = var.api_id
   resource_id   = aws_api_gateway_resource.resource[0].id
   http_method   = each.key
-  authorization = ((lookup(var.parameters, "authorizer", false) == true) || (lookup(var.parameters, "authorizer_id", null) != null)) ? "CUSTOM" : lookup(var.authorizer_by_method, each.key, null) == null ? "NONE" : lookup(var.authorizer_by_method[each.key], "authorizer", false) == true ? "CUSTOM" : "NONE"
+  authorization = lookup(var.authorizer_by_method, each.key, null) != null ? lookup(var.authorizer_by_method[each.key], "authorizer", false) == true ? "CUSTOM" : "NONE" : ((lookup(var.parameters, "authorizer", false) == true) || (lookup(var.parameters, "authorizer_id", null) != null)) ? "CUSTOM" : "NONE"
   authorizer_id = lookup(var.authorizer_by_method, each.key, null) != null ? lookup(var.authorizer_by_method[each.key], "authorizer_id", null) != null ? var.authorizer_by_method[each.key].authorizer_id : var.authorizer_id != null ? var.authorizer_id : var.api_gateway_authorizer_id : null
 
   request_parameters = length(regexall("^\\{.*\\}$", basename(local.name))) == 0 ? null : {
@@ -42,6 +42,7 @@ resource "aws_api_gateway_integration" "integrations" {
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"
   uri                     = var.lambda_function_invoke_arn != null ? var.lambda_function_invoke_arn : var.lambda_function_invoke_arns_by_method[each.key]
+  timeout_milliseconds    = lookup(var.parameters, "timeout_milliseconds", null)
 
   request_parameters = basename(local.name) == "{proxy+}" ? null : length(regexall("^\\{.*\\}$", basename(local.name))) == 0 ? null : {
     "integration.request.path.${replace(trim(basename(local.name), "{}"), ".", "_")}" = "method.request.path.${replace(trim(basename(local.name), "{}"), ".", "_")}"
