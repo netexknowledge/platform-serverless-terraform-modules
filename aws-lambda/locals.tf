18a6ed19a2d149b12d4b3bdfb694ebc58a2dd4f3
Juan Ramón Sixto Anello <juan.sixto@netexlearning.net>
2025-03-21 13:54:00 +0100

    feat(aws-lambda)!: standardize the nomenclature

--- aws-lambda/locals.tf
+++ aws-lambda/locals.tf
@@ -9,9 +9,14 @@ data "aws_ssm_parameter" "datadog_apikey" {
 }
 
 locals {
-  product_name   = format("%s-%s", var.tags["product"], var.tags["environment"])
-  product_path   = format("%s/%s", var.tags["product"], var.tags["environment"])
-  resource_name  = format("%s-%s-%s-%s", var.tags["product"], var.tags["environment"], var.tags["project"], var.parameters.function_name)
-  resource_path  = format("%s/%s/%s/%s", var.tags["product"], var.tags["environment"], var.tags["project"], var.parameters.function_name)
+  product_name              = format("%s", var.tags["product"])
+  product_path              = format("%s", var.tags["product"])
+  product_name_multiregion  = format("%s-%s", var.tags["product"], var.tags["environment"])
+  product_path_multiregion  = format("%s/%s", var.tags["product"], var.tags["environment"])
+  resource_name             = format("%s-%s-%s", var.tags["product"], var.tags["project"], var.parameters.function_name)
+  resource_path             = format("%s/%s/%s", var.tags["product"], var.tags["project"], var.parameters.function_name)
+  resource_name_multiregion = format("%s-%s-%s-%s", var.tags["product"], var.tags["environment"], var.tags["project"], var.parameters.function_name)
+  resource_path_multiregion = format("%s/%s/%s/%s", var.tags["product"], var.tags["environment"], var.tags["project"], var.parameters.function_name)
+
   aws_account_id = data.aws_caller_identity.current.account_id
 }
