7cc5919eaa870bb8c07323f218841b9f047fad69
Juan Ramón Sixto Anello <juan.sixto@netexlearning.net>
2025-06-25 01:57:13 +0200

    fix(aws-apigateway-deployment): fix WAF to APIGW about NIST compliance

--- aws-apigateway-deployment/main.tf
+++ aws-apigateway-deployment/main.tf
@@ -126,6 +126,10 @@ resource "aws_apigatewayv2_api_mapping" "custom_mapping" {
   api_mapping_key = var.base_path
 }
 
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 data "aws_waf_web_acl" "ApiGatewayACL" {
   count = (var.tags["environment"] != "tmp") ? 1 : 0
 
@@ -136,5 +140,5 @@ resource "aws_wafv2_web_acl_association" "waf_association" {
   count = (var.tags["environment"] != "tmp") ? 1 : 0
 
   resource_arn = var.type == "REST" ? aws_api_gateway_stage.stage[0].arn : aws_apigatewayv2_stage.stage[0].arn
-  web_acl_arn  = data.aws_waf_web_acl.ApiGatewayACL.arn
+  web_acl_arn  = format("arn:aws:wafv2:%s:%s:webacl/%s", data.aws_region.current.name, data.aws_caller_identity.current.account_id, data.aws_waf_web_acl.ApiGatewayACL[0].id)
 }
